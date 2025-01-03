a994ecb81ae6265c0aee038d919af222cc3b651e
Mario Kamel <mario.medhat99@gmail.com>
2025-01-03 14:54:37 +0200

    connection to pg

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5"
+)
+
+type makhdoum struct {
+	Name     string
+	Address  string
+	Birthday time.Time
+}
+
+func main() {
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", "postgres", "postgres1234@", "127.0.0.1", "5432", "temp")
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var m1 makhdoum
+	row := conn.QueryRow(context.Background(), "SELECT * FROM makhdoum LIMIT 1;")
+	err = row.Scan(&m1.Name, &m1.Address, &m1.Birthday)
+	if err != nil {
+		fmt.Println("Error Fetching Details:", err)
+		return
+	}
+	fmt.Println(m1)
 }
